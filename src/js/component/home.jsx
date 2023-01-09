b841c7b0508f4c3e2911b6f17ea80816bc9cecb2
lexie70 <alexandracampos658@gmail.com>
2023-01-09 21:50:21 +0000

    estructura del form

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,29 @@
-import React from "react";
+import React ,{useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	const [task,setTask]= useState("")
+	const [newTask,setNewTask]= useState("")
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">iniciando formulario con react</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex justify-content-center">
+		<form>
+		<div className="mb-3">
+		  <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+		  <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/> 
 		</div>
+		<div className="mb-3">
+		  <input type="password" className="form-control" id="exampleInputPassword1"/>
+		</div>
+		<div className="mb-3 form-check">
+		  <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+		  <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+		</div>
+		<button type="submit" className="btn btn-primary">Submit</button>
+	  </form>
+	  </div>
 	);
 };
 
