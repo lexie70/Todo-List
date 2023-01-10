9949620a2a2969122a128dac37d2bbae54cbf1d5
lexie70 <alexandracampos658@gmail.com>
2023-01-10 02:47:04 +0000

    creando la lista

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,26 +4,34 @@ import React ,{useState} from "react";
 
 //create your first component
 const Home = () => {
-	const [task,setTask]= useState("")
-	const [newTask,setNewTask]= useState("")
-	
+	const [todo,setTodo]= useState("")
+	const [list,setList]= useState([])
+// preventDefault ignora comportamiento de etiquetas predeterminadas
+	function submit(e){
+     e.preventDefault()
+	 if (todo === "") {
+		alert(add)
+		
+	 }else{
+		setList([...list, todo]);
+		setTodo("");
+	 }
+	}
 	return (
-		<div className="d-flex justify-content-center">
-		<form>
-		<div className="mb-3">
-		  <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-		  <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/> 
-		</div>
-		<div className="mb-3">
-		  <input type="password" className="form-control" id="exampleInputPassword1"/>
-		</div>
-		<div className="mb-3 form-check">
-		  <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-		  <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+		<div>
+			<h1 className="d-flex justify-content-center">todos</h1>
+			{/*div contenedor de form */}
+		<div className="d-flex justify-content-center">	
+		<form onSubmit={submit}>
+	    <div className="mb-3">
+		  {/*  input */}
+		  <input className="form-control" placeholder="What needs to be done?"  onChange={(e)=> setTodo (e.target.value)}/>
+          
 		</div>
 		<button type="submit" className="btn btn-primary">Submit</button>
 	  </form>
-	  </div>
+    </div> 
+</div>   
 	);
 };
 
