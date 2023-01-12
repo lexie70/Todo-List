9898a8d9462b88ae70f1231f6426b2c122c87482
lexie70 <alexandracampos658@gmail.com>
2023-01-12 03:33:44 +0000

    agregar informacion a la lista

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,32 +7,47 @@ const Home = () => {
 	const [todo,setTodo]= useState("")
 	const [list,setList]= useState([])
 // preventDefault ignora comportamiento de etiquetas predeterminadas
+
 	function submit(e){
      e.preventDefault()
 	 if (todo === "") {
-		alert(add)
+		alert("add")
 		
 	 }else{
 		setList([...list, todo]);
 		setTodo("");
 	 }
 	}
+	function equis(index){
+		let newList= list.filter((item,i)=> {if (index === i ) {
+			return item}
+		})
+		setList (newList)
+	}
+	
 	return (
 		<div>
 			<h1 className="d-flex justify-content-center">todos</h1>
-			{/*div contenedor de form */}
-		<div className="d-flex justify-content-center">	
-		<form onSubmit={submit}>
-	    <div className="mb-3">
-		  {/*  input */}
-		  <input className="form-control" placeholder="What needs to be done?"  onChange={(e)=> setTodo (e.target.value)}/>
-          
-		</div>
-		<button type="submit" className="btn btn-primary">Submit</button>
-	  </form>
-    </div> 
-</div>   
+			<div className="mb-3">
+				{/*  input */}
+				<input className="form-control" placeholder="What needs to be done?" value={todo}  onChange={(e)=> setTodo (e.target.value)}/>
+			</div>
+			<div className="d-flex justify-content-center">
+				<button type="submit" className="btn btn-primary " onClick={submit}>Submit</button>
+			</div>
+			{/* div contiene funcion map que hace que se muestre la lista del array que el usuario introduce */}
+			<div className="container text-center">
+				{list.map((item,index)=>(
+					<li className="mb-2" key={index}>{item}<span onClick={()=> equis(index)} className="ms-5 bg-secondary p-1 hover">X</span></li>	
+					))}
+			</div>
+		</div>   
 	);
 };
-
+			
 export default Home;
+				
+				
+
+	
+   
